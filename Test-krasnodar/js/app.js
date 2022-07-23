6616f1b0b6a5dbdc443e8bce16e65b24c3ff3c12
EgorKorolev26 <108061096+EgorKorolev26@users.noreply.github.com>
2022-07-23 20:40:56 +0300

    перазил
    допили кнопку бургер

--- Test-krasnodar/js/app.js
+++ Test-krasnodar/js/app.js
@@ -1,6 +1,13 @@
-const burger = document.querySelector("#burger");
-const menu = document.querySelector("#menu");
 
-burger.addEventListener("click", () => {
-	menu.classList.toggle("disp")
-});
+(function () {
+	const burger = document.querySelector('.burger');
+	burger.addEventListener('click', () => {
+		burger.classList.toggle('burger_active');
+	});
+	const menu = document.querySelector("#menu");
+
+	burger.addEventListener("click", () => {
+		menu.classList.toggle("disp")
+	});
+
+}());
