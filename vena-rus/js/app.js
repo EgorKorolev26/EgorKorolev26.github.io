77d90ff191ed288e4ff37a073eb1b4082611fd6f
EgorKorolev26 <108061096+EgorKorolev26@users.noreply.github.com>
2022-10-30 20:55:08 +0300

    меню
    меню сделал

--- vena-rus/js/app.js
+++ vena-rus/js/app.js
@@ -2,11 +2,6 @@ const prew = document.getElementById('prew');
 const next = document.getElementById('next');
 const sliderImages = document.getElementById('slider-images');
 
-prew.addEventListener('click', function () {
-	for (let item of sliderImages) {
-		item.classList.toggle('red');
-	}
-});
 
 
 
@@ -23,4 +18,26 @@ info.addEventListener('click', function () {
 
 
 
+/* BURGER */
 
+/* const menuOpen = document.getElementById('burger-open');
+const menu = document.getElementById('menu');
+const menuClose = document.getElementById('menu-close');
+
+menuOpen.addEventListener('click', function () {
+	menu.classList.toggle('disp');
+});
+menuClose.addEventListener('click', function () {
+
+	menu.classList.toggle('disp');
+
+}); */
+
+document.getElementById('burger-open').addEventListener('click', () => {
+	document.getElementById('menu').classList.add('active');
+	document.getElementById('menu-close').classList.add('close-menu-active');
+})
+document.getElementById('menu-close').addEventListener('click', () => {
+	document.getElementById('menu').classList.remove('active');
+	document.getElementById('menu-close').classList.remove('close-menu-active')
+})
